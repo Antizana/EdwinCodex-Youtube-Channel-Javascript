86a345631072dfce48b9210044ebf6ee56ac6806
Edwin Villacis <newemail@example.comexit>
2022-04-08 22:04:28 -0500

    Use of This in Javascript for my YouTube Channel

---
+++ basics/q131UseThis.js
@@ -0,0 +1,21 @@
+let dog = {
+  sound: "meows",
+  bark() {
+    console.log(this.sound);
+  },
+};
+
+dog.sound = "bark";
+let dogSound = dog.bark; //here we are losing the context for this
+dogSound(); // undefined. The context for this was lost.
+
+dog.bark(); // grunt
+
+console.log(dogSound);
+/**
+ * In the browser console returns:
+ * ƒ bark() {
+    console.log(this.sound);
+  }
+  Where the `this` keyword is referring to nothing.
+ */
