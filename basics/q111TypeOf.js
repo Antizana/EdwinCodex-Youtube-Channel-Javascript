cca4ab00cd02cb1f0e1423ca5943ee9686c978da
Edwin Villacis <newemail@example.comexit>
2022-04-10 17:54:47 -0500

    Typeof in JavaScript

---
+++ basics/q111TypeOf.js
@@ -0,0 +1,11 @@
+console.log(typeof []); // object
+console.log(typeof "Hello"); // string
+console.log(typeof 11); // string
+console.log(typeof true); // string
+console.log(typeof {}); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+let a = undefined + 1;
+console.log(a); // NaN
+console.log(typeof a); // number
+console.log(typeof (undefined + 1)); // number
