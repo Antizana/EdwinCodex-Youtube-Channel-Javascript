998b5e469c6fa431ef07432389405a61f7132731
Edwin Villacis <newemail@example.comexit>
2022-04-10 15:25:54 -0500

    Optional Chaining in Javascript

---
+++ basics/q125OptionalChaining.js
@@ -0,0 +1,5 @@
+let obj = {};
+console.log(obj.a?.b); // undefined
+console.log(obj.?a.?b); // SyntaxError: Unexpected token '?'
+console.log(obj[a][b]); // Throws ReferenceError: a is not defined
+console.log(obj?.a.b); // Throws TypeError: Cannot read property 'b' of undefined
