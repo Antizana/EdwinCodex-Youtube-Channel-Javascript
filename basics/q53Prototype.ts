2078898a5498e76527530b15c3bbeaf6d430e1dd
Edwin Villacis <edwinvillacis@yahoo.com>
2022-04-24 22:44:15 -0500

    Prototypes

---
+++ basics/q53Prototype.ts
@@ -0,0 +1,7 @@
+function hello(){
+  console.log("Hello World");
+}
+
+console.log(hello.prototype);
+
+// {constructor: ƒ}constructor: ƒ hello()arguments: nullcaller: nulllength: 0name: "hello"prototype: {constructor: ƒ}[[FunctionLocation]]: VM47:1[[Prototype]]: ƒ ()[[Scopes]]: Scopes[2][[Prototype]]: Object
